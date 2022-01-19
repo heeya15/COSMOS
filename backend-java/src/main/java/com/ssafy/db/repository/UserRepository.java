312cf7b28990a4fc70ef12e4f77e4d925d79eb88
김광희 <hgi7201@naver.com>
2022-01-19 13:22:52 +0900

    add checkUserId

--- backend-java/src/main/java/com/ssafy/db/repository/UserRepository.java
+++ backend-java/src/main/java/com/ssafy/db/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+// Optional<T>는 null이 올수 있는 값을 감싸는 Wrapper클래스로, 참조하더라도 NPE가 발생하지 않도록 도와준다
 
 /**
  * 유저 모델 관련 디비 쿼리 생성을 위한 JPA Query Method 인터페이스 정의.
@@ -13,4 +14,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     // 아래와 같이, Query Method 인터페이스(반환값, 메소드명, 인자) 정의를 하면 자동으로 Query Method 구현됨.
     Optional<User> findByUserId(String userId);
+    boolean UserId(String userId);
 }
