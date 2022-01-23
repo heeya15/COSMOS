7083b72f3f4db7bbad808ffae07668520faed0a5
김광희 <hgi7201@naver.com>
2022-01-23 23:40:35 +0900

    add board CRUD

--- backend-java/src/main/java/com/nsnt/cosmos/api/request/UserUpdateDto.java
+++ backend-java/src/main/java/com/nsnt/cosmos/api/request/UserUpdateDto.java
@@ -1,9 +1,5 @@
 package com.nsnt.cosmos.api.request;
 
-import java.time.LocalDateTime;
-
-import com.nsnt.cosmos.db.entity.User;
-
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +11,6 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor /** Cannot construct instance of~ error를 해결해주었다.  파라미터가 없는 생성자 만들어줌* */ 
 public class UserUpdateDto {
-
 	@ApiModelProperty(name = "유저 id")
 	private String user_id;
 	@ApiModelProperty(name = "유저 Password")
@@ -24,11 +19,6 @@ public class UserUpdateDto {
 	@ApiModelProperty(name = "유저 이름")
 	private String user_name;
 
-	public UserUpdateDto(User entity) {
-		this.user_id = entity.getUserId();
-		this.user_name = entity.getUserName();
-		this.user_password = entity.getUserPassword();
-	}
 
 //    public User toEntity(){
 //        return User.builder()
