dc7a209f0f09859ed55afb04e5f094bb6bbf323d
문슬기 <tmfrlghtjr@gmail.com>
2022-01-25 15:37:04 +0900

    update board detail

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -13,6 +13,7 @@ export default new Vuex.Store({
   state: {
     userInfo:null,
     isLogin:false,
+    boardNo: null,
   },
   mutations: {
     SIGNUP(state,credentials){
@@ -21,6 +22,11 @@ export default new Vuex.Store({
     LOGIN(state){
       console.log( '로그인됨!!!!')
       state.isLogin = true
+    },
+    GET_BOARD_NO(state){
+      console.log(state)
+      console.log('번호 확인')
+      // state.boardNo
     }
   },
   actions: {
@@ -53,7 +59,20 @@ export default new Vuex.Store({
         console.log(err)
       })
     },
-    
+    getBoardNo({commit}, ) {
+      axios({
+        method: 'get',
+        url: 'http://localhost:8080/api/board/searchAll',
+        // headers: this.
+      })
+      .then(res => {
+        console.log(res)
+        commit('GET_BOARD_NO')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
   },
   modules: {
   }
