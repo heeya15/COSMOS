10a3a8b241ff145671ede0bc7858b1da380ba344
김광희 <hgi7201@naver.com>
2022-01-25 17:50:32 +0900

    add studymember UD

--- backend-java/src/main/java/com/nsnt/cosmos/api/service/StudyMemberService.java
+++ backend-java/src/main/java/com/nsnt/cosmos/api/service/StudyMemberService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import com.nsnt.cosmos.api.request.SaveBoardDto;
 import com.nsnt.cosmos.api.request.SaveStudyMemberDto;
-import com.nsnt.cosmos.db.entity.Board;
 import com.nsnt.cosmos.db.entity.StudyMember;
 
 
@@ -16,13 +15,16 @@ public interface StudyMemberService {
     public StudyMember insertStudyMember(SaveStudyMemberDto saveStudyMemberDto);
      
 
+    /** 스터디 멤버중 해당 studymember_no로 한 회원의 정보만 찾는 findOneStudyMember 입니다.*/
+    public StudyMember findOneStudyMember(Long studymember_no);
+    
     /** 해당 회원들의 정보를 가져오는 findStudyMmeber 입니다. (목록 부분에 사용)*/
     public List<StudyMember> findStudyMmeber(Long study_no);
 
     /** 스터디 멤버 점수 수정하는 updateStudyMemberScore 입니다.*/
-    public StudyMember updateStudyMemberScore(StudyMember studymember, SaveBoardDto saveBoardDto);
-      
-
+    public StudyMember updateStudyMemberScore(StudyMember studymember, SaveStudyMemberDto saveStudyMemberDto);
+           
+    
     /** 스터디 멤버 회원을삭제하는 deleteStudyMember 입니다. */
     public void deleteStudyMember(StudyMember studymember);
 }
