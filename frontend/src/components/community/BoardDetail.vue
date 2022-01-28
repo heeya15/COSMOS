d68f8251eef8bea134466362ea22f34d9d718fdc
문슬기 <tmfrlghtjr@gmail.com>
2022-01-28 11:14:24 +0900

    add comment create

--- frontend/src/components/community/BoardDetail.vue
+++ frontend/src/components/community/BoardDetail.vue
@@ -4,14 +4,23 @@
     <hr>
     <h3>상세보기</h3>
     <center>
-      <div class="p-5" style="width: 600px">
+      <!-- 수정 버튼 수정 모디파이 -->
+      <div class="p-5" style="width: 600px" v-if="editButton === false">
         <b-row>
+          <b-col cols="3" class="mt-2">
+            <p>제목</p>
+          </b-col>
+          <b-col cols="9" class="mt-2" >
+            <input v-if="editButton === true" type="text" v-model="boardInfo.contentTitle">
+            <!-- <p>{{ boardInfo.contentTitle }}</p> -->
+          </b-col>
+
           <b-col cols="3" class="mt-2">
             <p>스터디 이름</p>
           </b-col>
           <b-col cols="9" class="mt-2">
-            <p id="studyName_tag"><input type="text">{{ boardInfo.studyName }}</p>
-            
+            <input v-if="editButton === true" type="text" v-model="boardInfo.studyName">
+            <p v-else id="studyName_tag">{{ boardInfo.studyName }}</p>
           </b-col>
 
           <b-col cols="3" class="mt-2">
@@ -50,6 +59,8 @@
       <b-button v-else style="background-color: #DAC7F9">스터디 신청</b-button>
       <b-button style="background-color: #DAC7F9" @click="goBoardMain">목록</b-button>
       <b-button v-if="userInfo.user_id === loginUserId" style="background-color: #DAC7F9" @click="deleteBoardForm">삭제</b-button>
+      <!-- <b-button v-if="editButton === true" style="background-color: #DAC7F9" @click="updateForm">저장</b-button> -->
+      <b-button v-if="editButton === true" style="background-color: #DAC7F9" @click="updateForm">수정</b-button>
     </div>
     <center>
       <b-row style="width: 600px;">
@@ -57,15 +68,13 @@
           <p>댓글</p>
         </b-col>
         <b-col cols="6" class="mt-3">
-          <b-textarea></b-textarea>
+          <b-input v-model="commentInput.content"></b-input>
         </b-col>
         <b-col cols="3" class="mt-3">
-          <b-button>작성</b-button>
+          <b-button @click="createComment">작성</b-button>
         </b-col>
       </b-row>
     </center>
-      <!-- {{userInfo.user_id}}
-      {{loginUserId }} -->
   </div>  
 </template>
 
@@ -76,6 +85,7 @@ export default {
   name: 'BoardDetail',
   data() {
     return {
+      editButton: false,
       loginUserId: null,
       board_no: this.$store.state.boardNo,
       boardInfo: {
@@ -92,6 +102,12 @@ export default {
       userInfo: {
         user_name: null,
         user_id: null,
+      },
+      commentInput: {
+        comment_no: null,
+        content: null,
+        created_at: null,
+        // user_id: null,
       }
     }
   },
@@ -107,7 +123,8 @@ export default {
       this.$router.push({name: 'MainBoard'})
     },
     boardFormEdit() {
-      this.$router.push({name: 'BoardFormUpdate'})
+      // this.$router.push({name: 'BoardFormUpdate'})
+      this.editButton = true
     },
     getBoard() {
       axios({
@@ -158,10 +175,71 @@ export default {
         console.log(err)
       })
     },
+
+    // 게시물 수정 
+    updateForm() {
+      const updateItem = {
+        board_no: this.boardInfo.boardNo,
+        content: this.boardInfo.content,
+        content_status: this.boardInfo.contentStatus,
+        content_title: this.boardInfo.contentTitle,
+        recruit_number: this.boardInfo.recruitNumber,
+        study_name: this.boardInfo.studyName,
+        studytype_name: this.boardInfo.studytypeName,
+        user_id: this.userInfo.user_id,
+      }
+      axios({
+        method: 'put',
+        url: 'http://i6e103.p.ssafy.io:8080/api/board/update',
+        data: updateItem,
+        headers: this.getToken(),
+      })
+      .then(() => {
+        this.getBoard()
+        this.study_name = this.boardInfo.studyName
+        this.editButton = false
+        // console.log(this.boardInfo)
+        // 500 에러 발생
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      this.boardInfo.studyName = null
+    },
+    createComment() {
+      const createCommentItem = {
+        board_no: this.boardInfo.boardNo,
+        // comment_no: this.commentInput.comment_no,
+        content: this.commentInput.content,
+        user_id: this.userInfo.user_id,
+      }
+      axios({
+        method: 'post',
+        url: 'http://i6e103.p.ssafy.io:8080/api/comment/register',
+        data: createCommentItem,
+        // headers: this.getToken()
+
+      })
+      .then(res => {
+        console.log('댓글부분')
+        console.log(res.config)
+        console.log(this.boardInfo.boardNo)
+        // console.log(this.commentInput.comment_no)
+        console.log(this.userInfo.user_id)
+        console.log(this.commentInput.content)
+        // this.$router.push({name: "MainBoard"})
+        console.log('댓글')
+        
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
   },
   created() {
     this.getBoard()
     this.getUserInfo()
+    // this.createComment()
   },
 }
 </script>
