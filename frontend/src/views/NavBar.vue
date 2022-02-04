450f77b74f27388cc2e6f51c2dca87eb96d3f93e
한성희 <bee1404@naver.com>
2022-02-04 23:40:18 +0900

    update login & singup css

--- frontend/src/views/NavBar.vue
+++ frontend/src/views/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="navPage">
     <!-- <b-navbar variant="faded" type="light">
       <b-navbar-brand tag="h1" class="mb-0">
         <router-link :to="{ name:'MainPage' }" class="text-decoration-none">COSMOS</router-link>
@@ -11,21 +11,21 @@
     </b-navbar> -->
     <nav class="navbar d-flex" id="nav">
       <a class="navbar-brand" href="/">
-        <img src="@/assets/cosmos_logo1.png" alt="cosmos" width="30" height="24">
-        <img src="@/assets/cosmos_logo2.png" alt="cosmos" width="30" height="24">
+        <!-- <img src="@/assets/cosmos_logo1.png" alt="cosmos" width="30" height="24"> -->
+        <img id="logo" src="@/assets/cosmos_logo3.png" alt="cosmos">
         <!-- <router-link :to="{ name:'MainPage' }" class="text-decoration-none">COSMOS</router-link> -->
       </a>
-       <router-link :to="{ name:'Openvidu' }" class="text-decoration-none">openviduTest</router-link>
+      <router-link :to="{ name:'Openvidu' }" class="nav-text text-decoration-none">openviduTest</router-link>
       <!-- <div class="nav ml-auto" v-if="!isLogin"> -->
-      <div class="nav ml-auto align-items-center" v-if="this.$store.state.isLogin">
-        <router-link :to="{ name:'MainBoard' }" class="text-decoration-none mx-1">스터디 모집</router-link>
-        <router-link :to="{ name:'StudyRoomCreateForm' }" class="text-decoration-none mx-1">스터디 방 생성</router-link>
-        <router-link :to="{ name:'MyPage' }" class="text-decoration-none mx-1">마이페이지</router-link>
+      <div class="nav ml-5 align-items-center" v-if="this.$store.state.isLogin">
+        <router-link :to="{ name:'MainBoard' }" class="nav-text text-decoration-none mx-1">스터디 모집</router-link>
+        <router-link :to="{ name:'StudyRoomCreateForm' }" class="nav-text text-decoration-none mx-1">스터디 방 생성</router-link>
+        <router-link :to="{ name:'MyPage' }" class="nav-text text-decoration-none mx-1">마이페이지</router-link>
         <b-button class="m-1" @click="logOut">로그아웃</b-button>
       </div>
-      <div class="nav ml-auto" v-else>
-        <router-link :to="{ name:'SignUp' }" class="text-decoration-none mx-1">회원가입</router-link>
-        <router-link :to="{ name:'LogIn' }" class="text-decoration-none mx-1">로그인</router-link>
+      <div class="nav ml-5" v-else>
+        <router-link :to="{ name:'SignUp' }" class="nav-text text-decoration-none mx-1">회원가입</router-link>
+        <router-link :to="{ name:'LogIn' }" class="nav-text text-decoration-none mx-1">로그인</router-link>
       </div>
     </nav>
   </div>
@@ -45,8 +45,32 @@ export default {
 </script>
 
 <style scoped> 
+  #logo {
+    width: 55px;
+    height: 50px;
+  }
+
   img {
-    width: 60px;
-    height: 40px;
+    width: 100%;
+    height: 100%;
+  }
+
+  .navbar {
+    margin-bottom: 0;
+  }
+
+  #navPage {
+    height: 10%;
+    position: relative;
+    padding: 0;
+  }
+
+  #nav {
+    top: 50%;
+    transform: translate(0%,-50%);
+  }
+
+  .nav-text {
+    font-size: 20px;
   }
 </style>
