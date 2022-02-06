e569907aee0cd9f0a824b5b73312c3ca16bee7c0
김광희 <hgi7201@naver.com>
2022-02-07 02:21:25 +0900

    update baseURL

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://i6e103.p.ssafy.io:8080/api",
+  baseURL: "https://i6e103.p.ssafy.io:8080/api",
   headers: {
     "Content-type": "application/json",
   },
