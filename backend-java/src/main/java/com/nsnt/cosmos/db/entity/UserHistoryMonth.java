834d345bcadd297ed15aa3b4003362c56c080727
한성희 <bee1404@naver.com>
2022-02-12 19:06:31 +0900

    add weekly & monthly userhistory

--- backend-java/src/main/java/com/nsnt/cosmos/db/entity/UserHistoryMonth.java
+++ backend-java/src/main/java/com/nsnt/cosmos/db/entity/UserHistoryMonth.java
@@ -1,10 +1,16 @@
 package com.nsnt.cosmos.db.entity;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,9 +27,11 @@ import lombok.Setter;
 @Table(name = "userhistory_month")
 public class UserHistoryMonth {
 	@EmbeddedId
-	UserHistoryMonthId userhistoryMonthId;
+	UserHistoryMonthId userHistoryMonthId;
 	
 	@Column(name = "total_time")
 	long totalTime;
-
+	
+	
+	
 }
