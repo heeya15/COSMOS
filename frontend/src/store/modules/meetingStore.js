57e11d15a0e8749e9936a0d9d78d738dbce9bb03
yoonjung1205 <yoonjung1205@naver.com>
2022-02-15 00:28:26 +0900

    fix public study setting mutation

--- frontend/src/store/modules/meetingStore.js
+++ frontend/src/store/modules/meetingStore.js
@@ -5,16 +5,20 @@ const meetingStore = {
     isvideo: false,
   },
   mutations: {
-    SET_ROOM_SETTIG_ISAUDIO(state, audio) {
-      state.isaudio = audio;
-      // 공개방 들어오면서 셋팅 값 뭐임?
-      console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>dsadsadwqrwqrwqrwq")
-      console.log(state.isaudio);
-    },
-    SET_ROOM_SETTIG_ISVIDIO(state, video) {
-      state.isvideo = video;
-      console.log(state.isvideo);
-    },
+    // SET_ROOM_SETTIG_ISAUDIO(state, audio) {
+    //   state.isaudio = audio;
+    //   // 공개방 들어오면서 셋팅 값 뭐임?
+    //   console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>dsadsadwqrwqrwqrwq")
+    //   console.log(state.isaudio);
+    // },
+    // SET_ROOM_SETTIG_ISVIDIO(state, video) {
+    //   state.isvideo = video;
+    //   console.log(state.isvideo);
+    // },
+    SET_ROOM_SETTING(state, settings) {
+      state.isaudio = settings.mic
+      state.isvideo = settings.cam
+    }
   }
 };
 
